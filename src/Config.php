2acc590dabbf4a7ecd717d648d7de6fd0ed33648
Caio Scatolino <caio.scatolino@outlook.com>
2022-10-25 16:50:52 -0300

    Iniciando o projeto com um mvc base

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/xampp/htdocs/Projeto_CMS/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'cms';
+    CONST DB_USER = 'root';
+    const DB_PASS = '';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
